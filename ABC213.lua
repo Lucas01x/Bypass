49bf3c33ffe3973ed6f64eb41156a91a4327ae69
Lucas01x <xzttyu214@gmail.com>
2026-06-04 00:21:43 +0700

    Create ABC213.lua

---
+++ ABC213.lua
@@ -0,0 +1,13 @@
+local reg = debug.getregistry()
+
+for _, thread in next, reg do
+    if typeof(thread) == "thread" then
+        local ThreadScript = getscriptfromthread(thread)
+        if ThreadScript and tostring(ThreadScript) == "Baka" then
+            print(_)
+            task.cancel(thread)
+            print(coroutine.status(thread))
+            --print(getscriptfromthread(thread))
+        end
+    end
+end
